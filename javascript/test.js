512463a169cec0656f33de35478febd1ad832402
Thong Dinh <ngu.tdn@gmail.com>
2020-08-02 16:46:27 +0700

    day laf vi du 1

---
+++ javascript/test.js
@@ -0,0 +1,10 @@
+alert('xin chao');
+//button 1
+function noName() {
+  document.getElementById('demo').innerHTML = 'thongdn';
+}
+
+// button 2
+function myName() {
+  document.getElementById('demo').innerHTML = 'ducnt';
+}
